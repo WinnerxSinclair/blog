06d6dd4ef74b5681f486e01e3e978464874f6e57
WinnerxSinclair <cobrasuperkek@gmail.com>
2025-09-08 18:43:30 -0500

    fix env

--- server/plugins/dbConnect.js
+++ server/plugins/dbConnect.js
@@ -12,8 +12,16 @@ const connectToDatabase = async () => {
 
   const config = useRuntimeConfig();
   try{
-    console.log(config.mongooseConnectionString);
-    await mongoose.connect(config.mongooseConnectionString);
+    const connectionString = config.mongooseConnectionString;
+    console.log('Raw connection string:', connectionString);
+    console.log('Connection string length:', connectionString?.length);
+    console.log('First 20 chars:', connectionString?.substring(0, 20));
+    console.log('Last 20 chars:', connectionString?.substring(connectionString.length - 20));
+    console.log('Contains mongodb+srv:', connectionString?.includes('mongodb+srv://'));
+    
+    // Check for invisible characters
+    console.log('Connection string bytes:', Buffer.from(connectionString || '', 'utf8'));
+    await mongoose.connect(connectionString);
     isConnected = true;
     console.log('connect to db');
   }catch(err){
